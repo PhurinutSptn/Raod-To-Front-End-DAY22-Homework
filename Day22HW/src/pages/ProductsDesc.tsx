6ad7ae8def2193f73f2c0847a5a02a02537ec04a
hedgehogkung <hedgehogkung.phu@gmail.com>
2023-06-05 04:01:42 +0700

    Update ProductsDesc.tsx
    add component ProductRating and ProductInfo to ProductDesc Page

--- Day22HW/src/pages/ProductsDesc.tsx
+++ Day22HW/src/pages/ProductsDesc.tsx
@@ -1,9 +1,18 @@
 import NavBar from '../components/NavBar'
+import ProductInfo from '../components/ProductInfo'
+import ProductRating from '../components/ProductRating'
 
 const ProductsDesc = () => {
   return (
     <>
       <NavBar/>
+      <div className="container">
+        <div className='topDesc'>
+          <img src="src/assets/Edifier WH950NB.webp" alt="Headphone Edifier WH950NB" />
+          <ProductRating/>
+          <ProductInfo/>
+        </div>
+      </div>
     </>
   )
 }

9d9927f0fd241ed7c51c17a0bf9f7a43e88044ed
hedgehogkung <hedgehogkung.phu@gmail.com>
2023-06-05 15:43:37 +0700

    Update ProductContainer.tsx
    add prop for product price

--- Day22HW/src/components/ProductContainer.tsx
+++ Day22HW/src/components/ProductContainer.tsx
@@ -8,49 +8,57 @@ const ProductContainer = () => {
               <ProductCard
                 image="src/assets/Edifier WH950NB.webp"
                 alter="Headphone Edifier WH950NB"
-                prod_name="Edifier WH950NB">
+                prod_name="Edifier WH950NB"
+                price="6,990฿">
                 หูฟังจากทาง Edifier ในรุ่น WH9500NB เป็นหูฟังไร้สายที่ออกแบบมาให้มีน้ำหนักเบา สวมใส่สบาย สามารถใช้งานได้อย่างหลากหลาย
               </ProductCard>
               <ProductCard
                 image="src/assets/Edifier WH950NB.webp"
                 alter="Headphone Edifier WH950NB"
-                prod_name="Edifier WH950NB">
+                prod_name="Edifier WH950NB"
+                price="6,990฿">
                 หูฟังจากทาง Edifier ในรุ่น WH9500NB เป็นหูฟังไร้สายที่ออกแบบมาให้มีน้ำหนักเบา สวมใส่สบาย สามารถใช้งานได้อย่างหลากหลาย
               </ProductCard>
               <ProductCard
                 image="src/assets/Edifier WH950NB.webp"
                 alter="Headphone Edifier WH950NB"
-                prod_name="Edifier WH950NB">
+                prod_name="Edifier WH950NB"
+                price="6,990฿">
                 หูฟังจากทาง Edifier ในรุ่น WH9500NB เป็นหูฟังไร้สายที่ออกแบบมาให้มีน้ำหนักเบา สวมใส่สบาย สามารถใช้งานได้อย่างหลากหลาย
               </ProductCard>
               <ProductCard
                 image="src/assets/Edifier WH950NB.webp"
                 alter="Headphone Edifier WH950NB"
-                prod_name="Edifier WH950NB">
+                prod_name="Edifier WH950NB"
+                price="6,990฿">
                 หูฟังจากทาง Edifier ในรุ่น WH9500NB เป็นหูฟังไร้สายที่ออกแบบมาให้มีน้ำหนักเบา สวมใส่สบาย สามารถใช้งานได้อย่างหลากหลาย
               </ProductCard>
               <ProductCard
                 image="src/assets/Edifier WH950NB.webp"
                 alter="Headphone Edifier WH950NB"
-                prod_name="Edifier WH950NB">
+                prod_name="Edifier WH950NB"
+                price="6,990฿">
                 หูฟังจากทาง Edifier ในรุ่น WH9500NB เป็นหูฟังไร้สายที่ออกแบบมาให้มีน้ำหนักเบา สวมใส่สบาย สามารถใช้งานได้อย่างหลากหลาย
               </ProductCard>
               <ProductCard
                 image="src/assets/Edifier WH950NB.webp"
                 alter="Headphone Edifier WH950NB"
-                prod_name="Edifier WH950NB">
+                prod_name="Edifier WH950NB"
+                price="6,990฿">
                 หูฟังจากทาง Edifier ในรุ่น WH9500NB เป็นหูฟังไร้สายที่ออกแบบมาให้มีน้ำหนักเบา สวมใส่สบาย สามารถใช้งานได้อย่างหลากหลาย
               </ProductCard>
               <ProductCard
                 image="src/assets/Edifier WH950NB.webp"
                 alter="Headphone Edifier WH950NB"
-                prod_name="Edifier WH950NB">
+                prod_name="Edifier WH950NB"
+                price="6,990฿">
                 หูฟังจากทาง Edifier ในรุ่น WH9500NB เป็นหูฟังไร้สายที่ออกแบบมาให้มีน้ำหนักเบา สวมใส่สบาย สามารถใช้งานได้อย่างหลากหลาย
               </ProductCard>
               <ProductCard
                 image="src/assets/Edifier WH950NB.webp"
                 alter="Headphone Edifier WH950NB"
-                prod_name="Edifier WH950NB">
+                prod_name="Edifier WH950NB"
+                price="6,990฿">
                 หูฟังจากทาง Edifier ในรุ่น WH9500NB เป็นหูฟังไร้สายที่ออกแบบมาให้มีน้ำหนักเบา สวมใส่สบาย สามารถใช้งานได้อย่างหลากหลาย
               </ProductCard>
           </div>
